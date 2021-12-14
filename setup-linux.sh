1d03ce38b672771cbd9c00145af9004de7c493f9
esen <yshen.chia@gmail.com>
2021-12-15 02:53:41 +0800

    feat: symlink tmux and vim configs to home dir

--- setup-linux.sh
+++ setup-linux.sh
@@ -10,5 +10,8 @@ cp -r dot-files/vim .vim/
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+ln -s .tmux/.tmux.conf .tmux.conf
+ln -s .vim/.vimrc .vimrc
+
 echo "[ -f ~/.bash_aliases ] && source ~/.bash_aliases" >> ~/.bashrc
 source ~/.bashrc
