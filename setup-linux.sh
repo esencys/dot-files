57afa980629e57dc847c600c9032752ff4216e84
Esen <yshen.chia@gmail.com>
2022-04-11 12:31:53 +0800

    feat: Use symlink for files, update nvm version and remove tpm from README

--- setup-linux.sh
+++ setup-linux.sh
@@ -2,16 +2,14 @@
 
 cd
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-
-cp -r dot-files/tmux .tmux/
-cp -r dot-files/bash/.bash_aliases .
-cp -r dot-files/vim .vim/
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-ln -s .tmux/.tmux.conf .tmux.conf
-ln -s .vim/.vimrc .vimrc
+ln -s ~/repos/dot-files/vim/.vimc ~/.vimrc
+ln -s ~/repos/dot-files/vim/coc-settings.json ~/.vim/coc-settings.json
+ln -s ~/repos/dot-files/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/repos/dot-files/bash/.bash_aliases ~/.bash_aliases
 
 echo "[ -f ~/.bash_aliases ] && source ~/.bash_aliases" >> ~/.bashrc
 source ~/.bashrc
--- vim/README.md
+++ vim/README.md
@@ -2,6 +2,3 @@
 
 CocInstall :
 `coc-vetur coc-json coc-tsserver coc-css coc-clangd coc-cmake coc-emmet coc-eslint coc-fsharp coc-html coc-jedi coc-omnisharp coc-prettier coc-rls`
-
-nvm :
-`curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash`
